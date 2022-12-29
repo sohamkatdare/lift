973a6a72a9be07e22263fc90808bc3164e9b4606
Saptak Das <66028584+Saptak625@users.noreply.github.com>
2022-12-29 14:30:38 -0500

    Fix Build Configuration and Speed Up Three.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
-export default {
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
     root: 'src',
     build: {
-        outDir: '../dist'
-    }
-}
+        outDir: '../dist',
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'src/index.html'),
+                info: resolve(__dirname, 'src/info/index.html'),
+            },
+        },
+    },
+})
