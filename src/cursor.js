98398804e5120ea8bf588c57b02ad683fca79db8
neel-kumar-v <greenworldwins@gmail.com>
2022-12-31 12:40:32 -0600

    Co-authored-by: SaptakD625 <Saptak625@users.noreply.github.com> Co-authored-by: Soham Katdare <sohamkatdare@users.noreply.github.com>

--- src/cursor.js
+++ src/cursor.js
@@ -1,14 +1,49 @@
 const customCursor = document.querySelector('#cursor');
+const customPointer = document.querySelector('#pointer');
 let isLeft = false;
 
 function lerp(start, end, amount) {
     return (1 - amount) * start + amount * end
 }
 
+function isTouchDevice() {
+    return (('ontouchstart' in window) ||
+       (navigator.maxTouchPoints > 0) ||
+       (navigator.msMaxTouchPoints > 0));
+}
+
+{/* <div class="fixed top-0 left-0 block pointer-events-none">
+    <!-- circle -->
+    <div class="absolute left-0 top-0 w-[30px] h-[30px] ml-[-15px] mt-[-15px]" id="cursor">
+      <div class="absolute left-0 top-0 w-full h-full box-border opacity-90 transition-opacity bg-gray-50 rounded-full scale-[0.08] bloom-cursor mt[-32px] ml-[28px]"  style="transform: translate3d(100px, 100px, 100px); contain: strict;">
+        <span class="text-gray-50 text-4xl" style="padding-top:80%"> <!--  -->
+          >
+        </span>
+      </div>
+    </div>
+    <!-- pointer so then ill delete this stuff?-->
+    <div class="arrow-pointer-container absolute left-0 top-0 " style="contain: layout strict;" id="pointer">
+        <div class="arrow-pointer-inner absolute left-0 top-0 transition-transform" >
+            <div class="arrow-pointer-body absolute left-0 top-0 w-[80px]  opacity-90 ">
+                <div class="js_img" data-img="1" data-append="1">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+  </div> */}
+
 const moveCursor = (e) => {
+    if(isTouchDevice()) {
+        console.log("hidden")
+        customCursor.style.display = "none";
+        customPointer.style.display = "none";
+        return
+    }
     const mouseY = e.clientY;
     const mouseX = e.clientX;
     customCursor.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 100px)`;
+    customPointer.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 100px)`;
     let width = window.innerWidth;
     isLeft = (mouseX <= width / 2);
 }
