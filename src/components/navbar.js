8008aec311031fac8c47e1408c57f066d1c9324c
neel-kumar-v <greenworldwins@gmail.com>
2023-01-01 11:08:14 -0600

    Glass boxes, components
    Made the boxes in the main menu and the navbars have a backdrop-blur
    Created components for the navbar and footer so it can be copied and pasted easily

---
+++ src/components/navbar.js
@@ -0,0 +1,42 @@
+class Navbar extends HTMLElement {
+  constructor() {
+    super();
+  }
+  connectedCallback() {
+    this.innerHTML = `
+      <header class="navbar fixed bg-base-200/25 backdrop-blur-sm z-50">
+        <div class="navbar-start">
+          <div class="dropdown">
+            <label tabindex="0" class="btn btn-ghost btn-circle">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewbox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" />
+              </svg>
+            </label>
+            <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+              <li><a href="../">Homepage</a></li>
+              <li><a href="info/">About</a></li>
+              <li><a href="products/">Products</a></li>
+              <li><a href="promotions/">Promotions</a></li>
+              <li><a href="launch/">Launch</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="navbar-center">
+          <a class="btn btn-ghost normal-case text-2xl">LIFT</a>
+        </div>
+        <div class="navbar-end">
+          <button class="btn btn-ghost btn-circle">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewbox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
+          </button>
+        </div>
+      </header>
+    `;
+  }
+}
+
+customElements.define('navbar-component', Navbar);
+
+
