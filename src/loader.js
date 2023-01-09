99369a6549b2da5f0dd461694ec35d678d027a64
neel-kumar-v <79852376+neel-kumar-v@users.noreply.github.com>
2023-01-09 01:26:26 +0000

    Build preloader, Responsive navbar fixed fonts loading incorrectly

---
+++ src/loader.js
@@ -0,0 +1,13 @@
+const preloader = document.getElementById('preloader');
+
+const fadeEffect = setInterval(() => {
+    // if we don't set opacity 1 in CSS, then it will be equaled to "" -- that's why we check it, and if so, set opacity to 1
+    if (!preloader.style.opacity) preloader.style.opacity = 1;
+    if (preloader.style.opacity > 0) {
+       preloader.style.opacity -= 0.1; 
+       preloader.classList += ' hidden'
+    } 
+    else clearInterval(fadeEffect);
+}, 100);
+
+window.addEventListener('load', fadeEffect);
--- src/navbar.js
+++ src/navbar.js
@@ -4,18 +4,21 @@ class Navbar extends HTMLElement {
   }
   connectedCallback() {
     this.innerHTML = `
-      <header class="navbar fixed bg-base-200/[0.125] backdrop-blur-sm z-50">
+      <nav class="navbar fixed bg-base-200/[0.125] backdrop-blur-sm z-50">
         <div class="navbar-start w-[47.5%]">
-          <div class="flex-1">
-            <ul class="menu menu-horizontal px-1">
-              <li><a href="../">Home</a></li>
+          <div class="dropdown">
+            <label tabindex="0" class="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </label>
+            <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+              <li><a href="/">Home</a></li>
               <li><a href="../info/">About</a></li>
               <li tabindex="0">
-                <a href="../launch/">
+                <a class="justify-between">
                   Launch
-                  <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+                  <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
                 </a>
-                <ul class="p-2 bg-white-100/50 backdrop-blur-md">
+                <ul class="p-2">
                   <li><a href="../launch/vehicles/">Vehicles</a></li>
                   <li><a href="../launch/training/">Training</a></li>
                   <li><a href="../launch/safety/">Safety</a></li>
@@ -26,23 +29,45 @@ class Navbar extends HTMLElement {
               <li><a href="../references/">References</a></li>
             </ul>
           </div>
+          <ul class="menu menu-horizontal px-1 hidden lg:flex">
+            <li><a href="/">Home</a></li>
+            <li><a href="../info/">About</a></li>
+            <li tabindex="0">
+              <a>
+                Launch
+                <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+              </a>
+              <ul class="p-2">
+                <li><a href="../launch/vehicles/">Vehicles</a></li>
+                <li><a href="../launch/training/">Training</a></li>
+                <li><a href="../launch/safety/">Safety</a></li>
+                <li><a href="../launch/recovery/">Recovery</a></li>
+              </ul>
+            </li>
+            <li><a href="../products/">Products</a></li>
+            <li><a href="../references/">References</a></li>
+          </ul>
+          
         </div>
         <div class="navbar-center">
-          <a class="btn btn-ghost normal-case text-4xl tracking-wide" href="../" style="font-family: Nova;"><code>LIFT</code></a>
+          <a class="btn btn-ghost normal-case text-xl" href="/"><code>LIFT</code></a>
         </div>
-      </header>
-      <header class="navbar fixed bg-base-200/[0.125] backdrop-blur-sm z-50">
+      </nav>
+      <nav class="navbar fixed bg-base-200/[0.125] backdrop-blur-sm z-50">
         <div class="navbar-start w-[47.5%]">
-          <div class="flex-1">
-            <ul class="menu menu-horizontal px-1">
+          <div class="dropdown">
+            <label tabindex="0" class="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </label>
+            <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
               <li><a href="/">Home</a></li>
-              <li><a href="/info/">About</a></li>
+              <li><a href="../info/">About</a></li>
               <li tabindex="0">
-                <a href="/launch/">
+                <a class="justify-between">
                   Launch
-                  <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+                  <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
                 </a>
-                <ul class="p-2 bg-white-100/50 backdrop-blur-md">
+                <ul class="p-2">
                   <li><a href="../launch/vehicles/">Vehicles</a></li>
                   <li><a href="../launch/training/">Training</a></li>
                   <li><a href="../launch/safety/">Safety</a></li>
@@ -53,16 +78,37 @@ class Navbar extends HTMLElement {
               <li><a href="../references/">References</a></li>
             </ul>
           </div>
+          <ul class="menu menu-horizontal px-1 hidden lg:flex">
+            <li><a href="/">Home</a></li>
+            <li><a href="../info/">About</a></li>
+            <li tabindex="0">
+              <a>
+                Launch
+                <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+              </a>
+              <ul class="p-2">
+                <li><a href="../launch/vehicles/">Vehicles</a></li>
+                <li><a href="../launch/training/">Training</a></li>
+                <li><a href="../launch/safety/">Safety</a></li>
+                <li><a href="../launch/recovery/">Recovery</a></li>
+              </ul>
+            </li>
+            <li><a href="../products/">Products</a></li>
+            <li><a href="../references/">References</a></li>
+          </ul>
+          
         </div>
         <div class="navbar-center">
-          <a class="btn btn-ghost normal-case text-4xl tracking-wide" href="../" style="font-family: Nova;"><code>LIFT</code></a>
+          <a class="btn btn-ghost normal-case text-xl" href="/"><code>LIFT</code> </a>
         </div>
-      </header>
+      </nav>
     `;
   }
 }
 
 customElements.define('navbar-component', Navbar);
+
+
 
 class NavbarLaunch extends HTMLElement {
   constructor() {
@@ -70,45 +116,21 @@ class NavbarLaunch extends HTMLElement {
   }
   connectedCallback() {
     this.innerHTML = `
-      <header class="navbar fixed bg-base-200/[0.125] backdrop-blur-sm z-50">
+      <nav class="navbar fixed bg-base-200/[0.125] backdrop-blur-sm z-50">
         <div class="navbar-start w-[47.5%]">
-          <div class="flex-1">
-            <ul class="menu menu-horizontal px-1">
+          <div class="dropdown">
+            <label tabindex="0" class="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </label>
+            <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
               <li><a href="/">Home</a></li>
               <li><a href="../../info/">About</a></li>
               <li tabindex="0">
-                <a href="../">
+                <a class="justify-between">
                   Launch
-                  <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+                  <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
                 </a>
-                <ul class="p-2 bg-white-100/50 backdrop-blur-md">
-                  <li><a href="../../launch/vehicles/">Vehicles</a></li>
-                  <li><a href="../../launch/training/">Training</a></li>
-                  <li><a href="../../launch/safety/">Safety</a></li>
-                  <li><a href="../../launch/recovery/">Recovery</a></li>
-                </ul>
-              </li>
-              <li><a href="../products/">Products</a></li>
-              <li><a href="../references/">References</a></li>
-            </ul>
-          </div>
-        </div>
-        <div class="navbar-center">
-          <a class="btn btn-ghost normal-case text-4xl tracking-wide" href="" style="font-family: Nova;"><code>LIFT</code></a>
-        </div>
-      </header>
-      <header class="navbar fixed bg-base-200/[0.125] backdrop-blur-sm z-50">
-        <div class="navbar-start w-[47.5%]">
-          <div class="flex-1">
-            <ul class="menu menu-horizontal px-1">
-              <li><a href="/">Home</a></li>
-              <li><a href="../../info/">About</a></li>
-              <li tabindex="0">
-                <a href="../">
-                  Launch
-                  <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-                </a>
-                <ul class="p-2 bg-white-100/50 backdrop-blur-md">
+                <ul class="p-2">
                   <li><a href="../../launch/vehicles/">Vehicles</a></li>
                   <li><a href="../../launch/training/">Training</a></li>
                   <li><a href="../../launch/safety/">Safety</a></li>
@@ -119,11 +141,79 @@ class NavbarLaunch extends HTMLElement {
               <li><a href="../../references/">References</a></li>
             </ul>
           </div>
+          <ul class="menu menu-horizontal px-1 hidden lg:flex">
+            <li><a href="/">Home</a></li>
+            <li><a href="../../info/">About</a></li>
+            <li tabindex="0">
+              <a>
+                Launch
+                <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+              </a>
+              <ul class="p-2">
+                <li><a href="../../launch/vehicles/">Vehicles</a></li>
+                <li><a href="../../launch/training/">Training</a></li>
+                <li><a href="../../launch/safety/">Safety</a></li>
+                <li><a href="../../launch/recovery/">Recovery</a></li>
+              </ul>
+            </li>
+            <li><a href="../../products/">Products</a></li>
+            <li><a href="../../references/">References</a></li>
+          </ul>
+          
         </div>
         <div class="navbar-center">
-          <a class="btn btn-ghost normal-case text-4xl tracking-wide" href="/" style="font-family: Nova;"><code>LIFT</code></a>
+          <a class="btn btn-ghost normal-case text-xl" href="/"><code>LIFT</code></a>
         </div>
-      </header>
+      </nav>
+      <nav class="navbar fixed bg-base-200/[0.125] backdrop-blur-sm z-50">
+        <div class="navbar-start w-[47.5%]">
+          <div class="dropdown">
+            <label tabindex="0" class="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </label>
+            <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+              <li><a href="/">Home</a></li>
+              <li><a href="../../info/">About</a></li>
+              <li tabindex="0">
+                <a class="justify-between">
+                  Launch
+                  <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
+                </a>
+                <ul class="p-2">
+                  <li><a href="../../launch/vehicles/">Vehicles</a></li>
+                  <li><a href="../../launch/training/">Training</a></li>
+                  <li><a href="../../launch/safety/">Safety</a></li>
+                  <li><a href="../../launch/recovery/">Recovery</a></li>
+                </ul>
+              </li>
+              <li><a href="../../products/">Products</a></li>
+              <li><a href="../../references/">References</a></li>
+            </ul>
+          </div>
+          <ul class="menu menu-horizontal px-1 hidden lg:flex">
+            <li><a href="/">Home</a></li>
+            <li><a href="../../info/">About</a></li>
+            <li tabindex="0">
+              <a>
+                Launch
+                <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+              </a>
+              <ul class="p-2">
+                <li><a href="../../launch/vehicles/">Vehicles</a></li>
+                <li><a href="../../launch/training/">Training</a></li>
+                <li><a href="../../launch/safety/">Safety</a></li>
+                <li><a href="../../launch/recovery/">Recovery</a></li>
+              </ul>
+            </li>
+            <li><a href="../../products/">Products</a></li>
+            <li><a href="../../references/">References</a></li>
+          </ul>
+          
+        </div>
+        <div class="navbar-center">
+          <a class="btn btn-ghost normal-case text-xl" href="/"><code>LIFT</code> </a>
+        </div>
+      </nav>
     `;
   }
 }
