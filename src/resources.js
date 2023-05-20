180ec8bcdaf82dc2e826fcca07713d62087d87c4
SaptakDnth <66028584+Saptak625@users.noreply.github.com>
2023-05-20 12:42:05 -0400

    Revert "animations"
    This reverts commit 9cdfad1873f97e210a4938230b4b805422722160.

--- src/resources.js
+++ src/resources.js
@@ -15,7 +15,6 @@ export function sceneSetup(backgroundPath) {
     // renderer.setClearColor(0xffffff, 0) // makes the background match
     return scene;
 }
-
 export function cameraSetup(scene, fov, aspect, near, far) {
     const ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
     scene.add(ambientLight);
@@ -23,11 +22,9 @@ export function cameraSetup(scene, fov, aspect, near, far) {
     camera.position.setZ(0);
     return camera;
 }
-
 export function isTouchDevice() {
     return (('ontouchstart' in window) || (navigator.msMaxTouchPoints > 0));
 }
-
 // renderer setup
 export function rendererSetup(scene, camera) {
     const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector("#bg"), antialias: true });
